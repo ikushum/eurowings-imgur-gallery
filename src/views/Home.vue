d3c7d78a1f83815256894624313064a26a09c0bb
Ishan Subedi <ikushum@gmail.com>
2020-11-16 14:54:16 +0545

    implement all filters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,93 @@
 <template>
   <v-container>
+    <div class="my-10">
+      <div class="d-flex align-center mb-5">
+        <label class="white--text mr-3">
+          Show Viral Images
+        </label>
+
+        <v-checkbox
+          v-model="filters.isShowViral"
+          dark
+          hide-details
+          class="pa-0 ma-0"
+          :disabled="isLoading"
+          @change="resetAndFetch()"
+        />
+      </div>
+
+      <div class="mb-5">
+        <label class="white--text mr-3">
+          Section:
+        </label>
+
+        <v-btn-toggle
+          v-model="filters.selectedSection"
+          dark
+          @change="resetAndFetch()"
+        >
+          <v-btn
+            v-for="sectionOption in filterOptions.sectionOptions"
+            :key="sectionOption"
+            dark
+            small
+            :disabled="isLoading"
+            :value="sectionOption"
+          >
+            {{ sectionOption }}
+          </v-btn>
+        </v-btn-toggle>
+      </div>
+
+      <div class="mb-5">
+        <label class="white--text mr-3">
+          Window:
+        </label>
+
+        <v-btn-toggle
+          v-model="filters.selectedWindow"
+          dark
+          @change="resetAndFetch()"
+        >
+          <v-btn
+            v-for="windowOption in filterOptions.windowOptions"
+            :key="windowOption"
+            dark
+            small
+            :disabled="isLoading"
+            :value="windowOption"
+          >
+            {{ windowOption }}
+          </v-btn>
+        </v-btn-toggle>
+      </div>
+
+      <div class="mb-5">
+        <label class="white--text mr-3">
+          Sort By:
+        </label>
+
+        <v-btn-toggle
+          v-model="filters.selectedSort"
+          dark
+          @change="resetAndFetch()"
+        >
+          <v-btn
+            v-for="sortOption in filterOptions.sortOptions"
+            :key="sortOption"
+            dark
+            small
+            :disabled="isLoading || filters.selectedSection !== 'user'"
+            :value="sortOption"
+          >
+            {{ sortOption }}
+          </v-btn>
+        </v-btn-toggle>
+      </div>
+    </div>
+
     <ImageGallery
+      class="my-10"
       :is-loading="isLoading"
       :galleries="galleries"
     />
@@ -15,10 +102,19 @@ export default {
   components: { ImageGallery },
   data() {
     return {
+      filterOptions: {
+        sectionOptions: ['hot', 'top', 'user'],
+        sortOptions: ['viral', 'top', 'time', 'rising'],
+        windowOptions: ['day', 'week', 'month', 'year', 'all'],
+      },
       isLoading: false,
+      filters: {
+        selectedSection: 'top',
+        selectedSort: 'viral',
+        isShowViral: true,
+        selectedWindow: 'day',
+      },
       galleries: [],
-      section: 'top',
-      sort: 'viral',
       page: 0,
       window: 'day',
       perPage: 10,
@@ -29,14 +125,16 @@ export default {
     this.handleInfiniteScroll();
   },
   methods: {
+    resetAndFetch() {
+      this.galleries = [];
+      this.fetchImages();
+    },
     fetchImages() {
       this.isLoading = true;
-      const url = `/gallery/${this.section}/${this.sort}/${this.window}/${this.page}`;
+      const url = `/gallery/${this.filters.selectedSection}/${this.filters.selectedSort}/${this.filters.selectedWindow}/${this.page}`;
 
       const params = {
-        showViral: true,
-        mature: true,
-        album_previews: true,
+        showViral: this.filters.isShowViral,
       };
 
       this.axios
